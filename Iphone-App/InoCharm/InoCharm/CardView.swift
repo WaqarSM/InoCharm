e78365ddf09c9df337a39809a5518a9a08641bfe
Waqar Muhammad <muhammad.s.waqar@gmail.com>
2023-02-11 00:12:54 -0500

    more learnings for ios app dev

---
+++ Iphone-App/InoCharm/InoCharm/CardView.swift
@@ -0,0 +1,40 @@
+//
+//  CardView.swift
+//  InoCharm
+//
+//  Created by Waqar Muhammad on 2023-02-06.
+//
+
+import SwiftUI
+
+
+struct CardView: View{
+    let scrum: DailyScrum
+    var body: some View{
+        VStack(alignment: .leading){
+            Text(scrum.title)
+                .font(.headline)
+                .accessibilityAddTraits(.isHeader)
+            Spacer()
+            HStack(){
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
+                    .padding(.trailing, 20)
+            }
+            .font(.caption)
+        }
+    }
+}
+
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
---
+++ Iphone-App/InoCharm/InoCharm/Models/DailyScrum.swift
@@ -0,0 +1,28 @@
+//
+//  DailyScrum.swift
+//  InoCharm
+//
+//  Created by Waqar Muhammad on 2023-02-06.
+//
+
+import Foundation
+
+
+struct DailyScrum{
+    var title:String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, theme: .yellow),
+        DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
+    ]
+}
+
+
